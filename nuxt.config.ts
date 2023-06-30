a9748e84d40e4a564a8e04ed1c3ba2ba9d626e8f
ashcolor <ashcolor27@gmail.com>
2023-06-30 13:39:10 +0900

    add: devツール設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,23 +5,30 @@ export default defineNuxtConfig({
     build: {},
     modules: [
         "@nuxtjs/tailwindcss",
-        "@pinia/nuxt",
-        "@vueuse/nuxt",
         "@nuxtjs/eslint-module",
         "@nuxt/content",
+        "@pinia/nuxt",
+        "@vueuse/nuxt",
     ],
+
     components: {
         dirs: ["~/components/content", "~/components/pages", "~/components/parts"],
     },
-    eslint: {
-        // yarn dev開始にlintを実行するか
-        lintOnStart: false,
-    },
+
     content: {
         // https://content.nuxtjs.org/api/configuration
         documentDriven: true,
         markdown: {
             remarkPlugins: ["remark-breaks"],
         },
+    },
+
+    eslint: {
+        // yarn dev開始にlintを実行するか
+        lintOnStart: false,
+    },
+
+    devtools: {
+        enabled: false,
     },
 });
