c22bd7adee71baacdef34f05c0a2bcdfa276e2ac
ashcolor <ashcolor27@gmail.com>
2023-06-30 13:43:10 +0900

    add: レイアウト

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <CommonHeader></CommonHeader>
+        <main class="container mx-auto flex flex-col px-4 py-12 sm:px-6 md:px-8">
+            <slot />
+        </main>
+        <CommonFooter></CommonFooter>
+    </div>
+</template>
