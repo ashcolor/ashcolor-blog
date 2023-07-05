b145a0807ffb9db9a8d925ca9bd96bdd8b1409eb
ashcolor <ashcolor27@gmail.com>
2023-07-05 15:43:34 +0900

    fix: defaultThemeの読み込み時にエラーになっていた問題の修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
