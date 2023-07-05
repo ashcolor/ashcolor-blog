13ba1cce8a13e4a35ab0bef446d04957fd2f0586
ashcolor <ashcolor27@gmail.com>
2023-07-05 15:42:11 +0900

    chg: コンテナの最大幅を短くする

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <CommonHeader></CommonHeader>
-        <main class="container mx-auto flex flex-col px-4 py-12 sm:px-6 md:px-8">
+        <main class="container mx-auto flex max-w-7xl flex-col px-4 py-12 sm:px-6 md:px-8">
             <slot />
         </main>
         <CommonFooter></CommonFooter>
