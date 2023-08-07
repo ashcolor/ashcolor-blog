ccc92e2d5485395a4c5ac069a0ab3a9b28d337e4
ashcolor <ashcolor27@gmail.com>
2023-08-07 14:50:37 +0900

    chg: padding調整

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,9 @@
         <header class="sticky top-0 z-50">
             <CommonHeader></CommonHeader>
         </header>
-        <main class="container mx-auto flex max-w-7xl flex-col px-4 py-12 sm:px-6 md:px-8">
+        <main
+            class="container mx-auto flex max-w-7xl flex-col px-4 py-6 sm:px-6 sm:py-8 md:px-8 md:py-12"
+        >
             <slot />
         </main>
         <CommonFooter></CommonFooter>
