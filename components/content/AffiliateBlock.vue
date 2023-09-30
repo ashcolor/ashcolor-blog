11f0475781e2a20823b30d94f51e49bb0a3d37a6
ashcolor <ashcolor27@gmail.com>
2023-09-30 10:15:35 +0900

    chg: アフィリエイト文言の追加

--- components/content/AffiliateBlock.vue
+++ components/content/AffiliateBlock.vue
@@ -58,6 +58,9 @@ const rakutenAffiliateUrl = computed(() => {
                     </a>
                 </span>
             </div>
+            <div>
+                <p class="text-xs">※アフィリエイトリンクを使用しています。</p>
+            </div>
         </div>
     </div>
 </template>
