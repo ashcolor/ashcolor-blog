71deaeaf94ad047c41c956b7bd1a5c0b1e3f160e
ashcolor <ashcolor27@gmail.com>
2023-11-20 21:56:13 +0900

    add: 記事

---
+++ components/content/LinkButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+    url: string;
+    title: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    url: "",
+    title: "",
+});
+</script>
+
+<template>
+    <NuxtLink :href="props.url" target="_blank" class="btn btn-primary btn-block my-4">
+        {{ props.title }}
+    </NuxtLink>
+</template>
