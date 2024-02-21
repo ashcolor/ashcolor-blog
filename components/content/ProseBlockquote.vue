ae6b6f1d40b10a7b6ffd8d2afed0b659d81becb0
ashcolor <ashcolor27@gmail.com>
2024-02-22 01:30:24 +0900

    chg: コントラスト調整

---
+++ components/content/ProseBlockquote.vue
@@ -0,0 +1,22 @@
+<template>
+    <blockquote class="prose-blockquote">
+        <slot />
+    </blockquote>
+</template>
+
+<style lang="ts" scoped>
+css({
+    blockquote: {
+        fontFamily: "{typography.font.body}",
+        margin: "{prose.blockquote.margin}",
+        paddingInlineStart: "{prose.blockquote.paddingInlineStart}",
+        quotes: "{prose.blockquote.quotes}",
+        color: "#666666",
+        borderInlineStartWidth: "{prose.blockquote.border.width}",
+        borderInlineStartStyle: "{prose.blockquote.border.style}",
+        borderInlineStartColor: "{prose.blockquote.border.color}",
+        "p:first-of-type::before": { content: "''" },
+        "p:last-of-type::after": { content: "''" },
+    },
+});
+</style>
