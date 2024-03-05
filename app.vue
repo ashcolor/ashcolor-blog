19cbf84dfeec31622a8f2672c03b72bd87f0b54a
ashcolor <ashcolor27@gmail.com>
2024-03-06 02:52:08 +0900

    add: NuxtPage Loading

--- app.vue
+++ app.vue
@@ -2,10 +2,15 @@
 useHead({
     htmlAttrs: { "data-theme": "mytheme" },
 });
+
+const { loading } = useNuxtPageLoading();
 </script>
 
 <template>
     <NuxtLayout>
+        <div v-show="loading" class="my-32 flex place-content-center">
+            <span class="loading loading-spinner loading-lg"></span>
+        </div>
         <!--  内部リンク遷移するとエラーになる問題の対応策 -->
         <!-- @see https://github.com/nuxt/nuxt/issues/13350#issuecomment-1397297746  -->
         <NuxtPage :key="$route.fullPath" />
