6d4c153d9ac06eaf3db3bae376f82906f75aeff0
ashcolor <ashcolor27@gmail.com>
2024-03-18 13:00:12 +0900

    chg: faviconサイズ

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,10 @@ export default defineNuxtConfig({
                 lang: "ja",
                 prefix: "og: http://ogp.me/ns#",
             },
+            link: [
+                { rel: "icon", href: "/favicon.ico" },
+                { rel: "apple-touch-icon", href: "/apple-touch-icon-180x180.png" },
+            ],
         },
     },
     css: ["@/assets/css/tailwind.css"],
