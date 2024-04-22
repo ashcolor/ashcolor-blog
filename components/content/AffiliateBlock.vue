a08e0e0a1f5207b83d1c9ab856633b832767d70e
ashcolor <ashcolor27@gmail.com>
2024-04-22 22:06:44 +0900

    fix: 動画や画像が別コンポーネントのものになる問題の修正

--- components/content/AffiliateBlock.vue
+++ components/content/AffiliateBlock.vue
@@ -43,50 +43,60 @@ const dlSiteCampaignText = () => {
 </script>
 
 <template>
-    <div
-        class="my-4 flex flex-col place-items-stretch gap-4 border p-4 sm:flex-row sm:items-center sm:justify-center"
-    >
-        <a
-            v-if="imageUrl"
-            :href="props.asin ? amazonUrl : dlSiteUrl"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="self-center"
+    <!-- 複数のカスタムコンポーネントを使用した際、
+        画像が別コンポーネントの画像に切り替わる不具合が出たためClientOnlyにしてfix -->
+    <ClientOnly>
+        <div
+            class="my-4 flex flex-col place-items-stretch gap-4 border p-4 sm:flex-row sm:items-center sm:justify-center"
         >
-            <img :src="props.imageUrl" :alt="props.title" class="max-w-48" loading="lazy" />
-        </a>
-        <div class="flex grow flex-col gap-2">
-            <div class="text-lg font-bold">
-                {{ props.title }}
-            </div>
-            <div class="flex flex-col gap-2 sm:flex-row">
-                <div v-if="props.asin" class="flex grow flex-col justify-end">
-                    <p v-if="amazonCampaignText()" class="text-center text-sm text-red-500">
-                        ＼&nbsp;{{ amazonCampaignText() }}&nbsp;／
-                    </p>
-                    <a
-                        class="btn w-full bg-[#ffd814] hover:bg-[#ffd814]"
-                        :href="amazonUrl"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Amazonで購入
-                    </a>
+            <a
+                v-if="imageUrl"
+                :href="props.asin ? amazonUrl : dlSiteUrl"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="self-center"
+            >
+                <img
+                    nuxt-client
+                    :src="props.imageUrl"
+                    :alt="props.title"
+                    class="max-w-48"
+                    loading="lazy"
+                />
+            </a>
+            <div class="flex grow flex-col gap-2">
+                <div class="text-lg font-bold">
+                    {{ props.title }}
                 </div>
-                <div v-if="props.dlSiteLink" class="flex grow flex-col justify-end">
-                    <p v-if="dlSiteCampaignText()" class="text-center text-sm text-red-500">
-                        ＼&nbsp;{{ dlSiteCampaignText() }}&nbsp;／
-                    </p>
-                    <a
-                        class="btn w-full bg-[#039] text-white hover:bg-[#039]"
-                        :href="dlSiteUrl"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        DLsiteで購入
-                    </a>
+                <div class="flex flex-col gap-2 sm:flex-row">
+                    <div v-if="props.asin" class="flex grow flex-col justify-end">
+                        <p v-if="amazonCampaignText()" class="text-center text-sm text-red-500">
+                            ＼&nbsp;{{ amazonCampaignText() }}&nbsp;／
+                        </p>
+                        <a
+                            class="btn w-full bg-[#ffd814] hover:bg-[#ffd814]"
+                            :href="amazonUrl"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Amazonで購入
+                        </a>
+                    </div>
+                    <div v-if="props.dlSiteLink" class="flex grow flex-col justify-end">
+                        <p v-if="dlSiteCampaignText()" class="text-center text-sm text-red-500">
+                            ＼&nbsp;{{ dlSiteCampaignText() }}&nbsp;／
+                        </p>
+                        <a
+                            class="btn w-full bg-[#039] text-white hover:bg-[#039]"
+                            :href="dlSiteUrl"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            DLsiteで購入
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </ClientOnly>
 </template>
--- components/content/YouTubeIframe.vue
+++ components/content/YouTubeIframe.vue
@@ -14,20 +14,24 @@ const props = withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-    <!-- TODO アスペクト比から%を計算する -->
-    <div class="my-8 flex flex-col place-items-center gap-2">
-        <div class="relative w-full pb-[56.25%]">
-            <!-- @https://www.ankursheel.com/blog/full-width-you-tube-video-embed -->
-            <iframe
-                class="absolute m-auto size-full"
-                :src="`https://www.youtube.com/embed/${props.videoId}?start=${props.start}`"
-                title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowfullscreen
-                loading="lazy"
-            ></iframe>
+    <!-- 複数のカスタムコンポーネントを使用した際、
+        動画が別コンポーネントの動画に切り替わる不具合が出たためClientOnlyにしてfix -->
+    <ClientOnly>
+        <!-- TODO アスペクト比から%を計算する -->
+        <div class="my-8 flex flex-col place-items-center gap-2">
+            <div class="relative w-full pb-[56.25%]">
+                <!-- @https://www.ankursheel.com/blog/full-width-you-tube-video-embed -->
+                <iframe
+                    class="absolute m-auto size-full"
+                    :src="`https://www.youtube.com/embed/${props.videoId}?start=${props.start}`"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                    loading="lazy"
+                ></iframe>
+            </div>
+            <p v-if="props.title" class="text-sm font-thin">{{ props.title }}</p>
         </div>
-        <p v-if="props.title" class="text-sm font-thin">{{ props.title }}</p>
-    </div>
+    </ClientOnly>
 </template>
