12f2ac256192168709d632950bf977e582ee3ac6
ashcolor <ashcolor27@gmail.com>
2024-07-16 10:48:40 +0900

    chg: 記事修正

--- components/content-feature/dtm/synthesizer-v/SingerTable.vue
+++ components/content-feature/dtm/synthesizer-v/SingerTable.vue
@@ -105,7 +105,7 @@ const filteredSingers = computed(() => {
                                     <a
                                         v-if="singer.dlsite_url"
                                         :href="Util.affiliateDlSiteUrl(singer.dlsite_url)"
-                                        class="ext-left link tooltip tooltip-accent"
+                                        class="link tooltip tooltip-info"
                                         data-tip="DLsiteで購入"
                                         target="_blank"
                                     >
