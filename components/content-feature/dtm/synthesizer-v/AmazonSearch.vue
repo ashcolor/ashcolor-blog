f271f0a4918061250f73dd2eb336aa3aefedc74b
ashcolor <ashcolor27@gmail.com>
2025-02-14 00:45:55 +0900

    chg: ClientOnly化

--- components/content-feature/dtm/synthesizer-v/AmazonSearch.vue
+++ components/content-feature/dtm/synthesizer-v/AmazonSearch.vue
@@ -3,24 +3,26 @@ const searchLink = "https://amzn.to/49nYcbu";
 </script>
 
 <template>
-    <div
-        class="my-4 flex flex-col place-items-stretch gap-4 border p-8 sm:flex-row sm:items-center sm:justify-center"
-    >
-        <div class="flex grow flex-col gap-2">
-            <!-- <div class="text-lg font-bold">DLsiteでSynthesizer V商品を検索</div> -->
-            <div class="flex flex-col gap-2 sm:flex-row">
-                <div class="flex grow flex-col justify-end">
-                    <a
-                        :href="searchLink"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        class="btn btn-lg w-full bg-[#ffd814] hover:bg-[#ffd814]"
-                    >
-                        <Icon name="bi:search"></Icon>
-                        Amazon で Synthesizer V を検索
-                    </a>
+    <ClientOnly>
+        <div
+            class="my-4 flex flex-col place-items-stretch gap-4 border p-8 sm:flex-row sm:items-center sm:justify-center"
+        >
+            <div class="flex grow flex-col gap-2">
+                <!-- <div class="text-lg font-bold">DLsiteでSynthesizer V商品を検索</div> -->
+                <div class="flex flex-col gap-2 sm:flex-row">
+                    <div class="flex grow flex-col justify-end">
+                        <a
+                            :href="searchLink"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            class="btn btn-lg w-full bg-[#ffd814] hover:bg-[#ffd814]"
+                        >
+                            <Icon name="bi:search"></Icon>
+                            Amazon で Synthesizer V を検索
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </ClientOnly>
 </template>
--- components/content-feature/dtm/synthesizer-v/DlSiteSearch.vue
+++ components/content-feature/dtm/synthesizer-v/DlSiteSearch.vue
@@ -5,23 +5,25 @@ const affiliateUrl = computed(() => Util.affiliateDlSiteUrl(searchLink));
 </script>
 
 <template>
-    <div
-        class="my-4 flex flex-col place-items-stretch gap-4 border p-8 sm:flex-row sm:items-center sm:justify-center"
-    >
-        <div class="flex grow flex-col gap-2">
-            <div class="flex flex-col gap-2 sm:flex-row">
-                <div class="flex grow flex-col justify-end">
-                    <a
-                        class="btn btn-lg w-full bg-[#039] text-white hover:bg-[#039]"
-                        :href="affiliateUrl"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Icon name="bi:search"></Icon>
-                        DLsite で Synthesizer V を検索
-                    </a>
+    <ClientOnly>
+        <div
+            class="my-4 flex flex-col place-items-stretch gap-4 border p-8 sm:flex-row sm:items-center sm:justify-center"
+        >
+            <div class="flex grow flex-col gap-2">
+                <div class="flex flex-col gap-2 sm:flex-row">
+                    <div class="flex grow flex-col justify-end">
+                        <a
+                            class="btn btn-lg w-full bg-[#039] text-white hover:bg-[#039]"
+                            :href="affiliateUrl"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Icon name="bi:search"></Icon>
+                            DLsite で Synthesizer V を検索
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </ClientOnly>
 </template>
