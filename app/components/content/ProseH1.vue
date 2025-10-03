d1c0265344ae31d5f06f40e7bf54d01afba55333
ashcolor <ashcolor27@gmail.com>
2025-10-03 23:33:30 +0900

    fix: レイアウトシフト

--- app/components/content/ProseH1.vue
+++ app/components/content/ProseH1.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { ClientOnly } from "#components";
 import { useRuntimeConfig } from "#imports";
 defineProps<{ id?: string }>();
 const heading = 1;
@@ -8,12 +7,10 @@ const generate = anchorLinks?.depth >= heading && !anchorLinks?.exclude.includes
 </script>
 
 <template>
-    <ClientOnly>
-        <h1 :id="id" class="my-4 py-2 text-2xl font-bold">
-            <a v-if="id && generate" :href="`#${id}`">
-                <slot />
-            </a>
-            <slot v-else />
-        </h1>
-    </ClientOnly>
+    <h1 :id="id" class="my-4 py-2 text-2xl font-bold">
+        <a v-if="id && generate" :href="`#${id}`">
+            <slot />
+        </a>
+        <slot v-else />
+    </h1>
 </template>
