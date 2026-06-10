8a232ef3eeafa8a965cc59d9eae71151ccf6b8b6
ashcolor <ashcolor27@gmail.com>
2026-06-10 22:09:46 +0900

    feat: YouTubeサムネに公式風の赤い再生ボタンを表示

--- app/components/content/YouTubeIframe.vue
+++ app/components/content/YouTubeIframe.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 interface Props {
     videoId: string;
-    title: string;
-    // TODO mdから呼ばれる際には常にstringになる
+    title?: string;
     start?: number | string;
 }
 
@@ -11,25 +10,47 @@ const props = withDefaults(defineProps<Props>(), {
     title: "",
     start: 0,
 });
+
+const playerVars = computed(() => ({
+    autoplay: 0,
+    playsinline: 1,
+    start: Number(props.start) || 0,
+}));
 </script>
 
 <template>
-    <!-- 複数のカスタムコンポーネントを使用した際、
-        動画が別コンポーネントの動画に切り替わる不具合が出たためClientOnlyにしてfix -->
     <ClientOnly>
-        <!-- TODO アスペクト比から%を計算する -->
         <div class="my-8 flex flex-col place-items-center gap-2">
-            <div class="relative w-full pb-[56.25%]">
-                <!-- @https://www.ankursheel.com/blog/full-width-you-tube-video-embed -->
-                <iframe
-                    class="absolute m-auto size-full"
-                    :src="`https://www.youtube.com/embed/${props.videoId}?start=${props.start}`"
-                    title="YouTube video player"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowfullscreen
-                    loading="lazy"
-                ></iframe>
+            <div class="relative w-full" style="aspect-ratio: 16/9">
+                <ScriptYouTubePlayer
+                    :video-id="props.videoId"
+                    :player-vars="playerVars"
+                    thumbnail-size="hqdefault"
+                    :webp="false"
+                    style="width: 100%; height: 100%"
+                >
+                    <template #placeholder="{ placeholder }">
+                        <div class="absolute inset-0">
+                            <img
+                                :src="placeholder"
+                                alt="動画のサムネイル"
+                                class="m-0 h-full w-full object-cover"
+                            />
+                            <!-- YouTube 公式風の再生ボタン -->
+                            <div
+                                class="absolute left-1/2 top-1/2 h-[48px] w-[68px] -translate-x-1/2 -translate-y-1/2"
+                            >
+                                <svg viewBox="0 0 68 48" width="68" height="48">
+                                    <path
+                                        fill="#f00"
+                                        d="M66.52 7.74c-.78-2.93-2.49-5.41-5.42-6.19C55.79.13 34 0 34 0S12.21.13 6.9 1.55c-2.93.78-4.63 3.26-5.42 6.19C.06 13.05 0 24 0 24s.06 10.95 1.48 16.26c.78 2.93 2.49 5.41 5.42 6.19C12.21 47.87 34 48 34 48s21.79-.13 27.1-1.55c2.93-.78 4.64-3.26 5.42-6.19C67.94 34.95 68 24 68 24s-.06-10.95-1.48-16.26z"
+                                    />
+                                    <path fill="#fff" d="M45 24 27 14v20" />
+                                </svg>
+                            </div>
+                        </div>
+                    </template>
+                </ScriptYouTubePlayer>
             </div>
             <p v-if="props.title" class="text-sm font-thin">{{ props.title }}</p>
         </div>
