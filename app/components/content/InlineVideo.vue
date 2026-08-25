51df231f22bfab7519d73f7ba8b681f70d3ea693
ashcolor <ashcolor27@gmail.com>
2026-08-25 23:39:27 +0900

    feat: ACE Studioのレビュー記事を追加

---
+++ app/components/content/InlineVideo.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+interface Props {
+    src: string;
+    title?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    title: "",
+});
+</script>
+
+<template>
+    <ClientOnly>
+        <div class="my-8 flex flex-col place-items-center gap-2">
+            <video
+                :src="props.src"
+                class="max-h-[48rem] min-h-0 grow-0 border"
+                autoplay
+                loop
+                muted
+                playsinline
+                controls
+            />
+            <p v-if="props.title" class="text-sm text-slate-500">{{ props.title }}</p>
+        </div>
+    </ClientOnly>
+</template>
---
+++ app/components/content/LinkButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+interface Props {
+    title?: string;
+    url: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    title: "",
+    url: "",
+});
+</script>
+
+<template>
+    <NuxtLink
+        :href="props.url"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="btn btn-primary my-4 w-full sm:w-auto"
+    >
+        {{ props.title }}
+    </NuxtLink>
+</template>
